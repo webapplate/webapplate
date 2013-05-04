ac087dbe62c6c01ebd18b70e52e1ca0f5ee7f901
gasolin <gasolin@gmail.com>
2013-05-04 16:15:13 +0800

    add sinon.js support in test framework

---
+++ public/test/unit/test.sinon.js
@@ -0,0 +1,6 @@
+// sinon test samples http://sinonjs.org/docs/
+suite('Sinon Tests', function(){
+	test('should be defined', function() {
+    	assert.notStrictEqual(sinon, undefined);
+    });
+});
