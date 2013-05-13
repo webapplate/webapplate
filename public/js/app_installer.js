fd38db02d56e173f9149deeef5ac0ae52b5de829
gasolin <gasolin@gmail.com>
2013-05-13 23:55:37 +0800

    update app_installer

--- public/js/app_installer.js
+++ public/js/app_installer.js
@@ -1,22 +1,25 @@
 //check if app is installed
-var manifestUrl = location.href.substring(0, location.href.lastIndexOf('/')) +
-    '/manifest.webapp';
-// alert(manifestUrl);
 if (navigator.mozApps != undefined) {
   var app_stat = navigator.mozApps.getSelf();
   app_stat.onsuccess = function() {
   if (app_stat.result) {
-        //instsalled
+    //instsalled
   } else {
-        // not installed
-        var app_install = navigator.mozApps.install(manifestUrl);
-        app_install.onsuccess = function() {
-        };
-        app_install.onerror = function() {
-        };
+    // not installed
+    var manifestUrl = location.href.substring(0,
+      location.href.lastIndexOf('/')) + '/manifest.webapp';
+    // alert(manifestUrl);
+
+    var app_install = navigator.mozApps.install(manifestUrl);
+    app_install.onsuccess = function() {
+    };
+    app_install.onerror = function() {
+    };
   }
-};
-app_stat.onerror = function() {
-  alert('Error checking installation status: ' + this.error.message);
-};
+  };
+  app_stat.onerror = function() {
+    alert('Error checking installation status: ' + this.error.message);
+  };
+} else {
+    console.log('WebApp not supported');
 }
