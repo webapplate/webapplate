969c4e06315fc4f09807385567190876fe872a71
gasolin <gasolin@gmail.com>
2013-11-15 16:32:18 +0800

    disable cache templates by default

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.configure(function(){
 	});
     app.set('views', __dirname + '/views');
     app.set('view options', { layout: false });
+    // Swig will cache templates for you, but you can disable
+    // that and use Express's caching instead, if you like:
     app.set('view cache', false);
+    // To disable Swig's cache, do the following:
+    swig.setDefaults({ cache: false });
+    // NOTE: You should always cache templates in a production environment.
+    // Don't leave both of these to `false` in production!
 
     // gzip
     app.use(express.compress());
