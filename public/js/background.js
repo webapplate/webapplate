5b16219d689f8fa8aeec3870aa9b57ecedc095f4
gasolin <gasolin@gmail.com>
2013-12-12 01:36:27 +0800

    basic chrome apps support

---
+++ public/js/background.js
@@ -0,0 +1,9 @@
+/* Only used in Chrome Apps */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('../index.html', {
+    'bounds': {
+      'width': 320,
+      'height': 480
+    }
+  });
+});
