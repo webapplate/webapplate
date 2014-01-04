8e4151c713ad0d8f79480a8fb8bf3c94a470c536
gasolin <gasolin@gmail.com>
2014-01-04 19:45:44 +0800

    fix app_installer.js lint via jslint

--- public/js/app_installer.js
+++ public/js/app_installer.js
@@ -1,42 +1,41 @@
-'use strict';
+(function () {
+  'use strict';
+  /**
+   * check if Firefox webapp is installed
+   *
+   * @class Installer
+   */
+  if (navigator.mozApps !== undefined) {
+    var app_stat = navigator.mozApps.getSelf();
+    app_stat.onsuccess = function () {
+      if (app_stat.result) {
+        //instsalled
+        var installed = navigator.mozApps.getInstalled();
+        installed.onsuccess = function () {
+          if (installed.result.manifest.version !==
+              app_stat.result.manifest.version) {
+            navigator.mozApps.install();
+          }
+        };
+        installed.onerror = function () {
+        };
+      } else {
+        // not installed
+        var manifestUrl = location.href.substring(0,
+          location.href.lastIndexOf('/')) + '/manifest.webapp';
+        // alert(manifestUrl);
 
-(function() {
-/**
- * check if Firefox webapp is installed
- *
- * @class Installer
- */
-if (navigator.mozApps !== undefined) {
-  var app_stat = navigator.mozApps.getSelf();
-  app_stat.onsuccess = function() {
-  if (app_stat.result) {
-    //instsalled
-    var installed = navigator.mozApps.getInstalled();
-    installed.onsuccess = function() {
-      if (installed.result.manifest.version !==
-          app_stat.result.manifest.version) {
-        navigator.mozApps.install();
+        var app_install = navigator.mozApps.install(manifestUrl);
+        app_install.onsuccess = function () {
+        };
+        app_install.onerror = function () {
+        };
       }
     };
-    installed.onerror = function() {
+    app_stat.onerror = function () {
+      alert('Error checking installation status: ' + this.error.message);
     };
   } else {
-    // not installed
-    var manifestUrl = location.href.substring(0,
-      location.href.lastIndexOf('/')) + '/manifest.webapp';
-    // alert(manifestUrl);
-
-    var app_install = navigator.mozApps.install(manifestUrl);
-    app_install.onsuccess = function() {
-    };
-    app_install.onerror = function() {
-    };
+      // console.log('WebApp not supported');
   }
-  };
-  app_stat.onerror = function() {
-    alert('Error checking installation status: ' + this.error.message);
-  };
-} else {
-    // console.log('WebApp not supported');
-}
 }());
