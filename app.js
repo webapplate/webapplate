7d41bbf2a31639518c46a6d06974daec1a493ab1
gasolin <gasolin@gmail.com>
2014-04-20 00:02:48 +0800

    #38 update express to 4.0.0

--- app.js
+++ app.js
@@ -1,59 +1,68 @@
-'use strict';
+/* global require, __dirname, process, console */
+(function() {
+  'use strict';
 
-var express = require('express');
-var swig = require('swig');
-var app = express();
+  var express = require('express');
+  var swig = require('swig');
+  var app = express();
+  var bodyParser = require('body-parser')();
+  var compress = require('compression')();
+  var serveStatic = require('serve-static');
+  var errorHandler = require('errorhandler');
 
-app.configure(function() {
-    // template
-    app.engine('.html', swig.renderFile);
-    app.set('view engine', 'html');
-    app.set('views', __dirname + '/views');
-    app.set('view options', { layout: false });
-    // Swig will cache templates for you, but you can disable
-    // that and use Express's caching instead, if you like:
-    app.set('view cache', false);
-    // To disable Swig's cache, do the following:
-    swig.setDefaults({ cache: false });
-    // NOTE: You should always cache templates in a production environment.
-    // Don't leave both of these to `false` in production!
+  // template
+  app.engine('.html', swig.renderFile);
+  app.set('view engine', 'html');
+  app.set('views', __dirname + '/views');
+  app.set('view options', { layout: false });
+  // Swig will cache templates for you, but you can disable
+  // that and use Express's caching instead, if you like:
+  app.set('view cache', false);
+  // To disable Swig's cache, do the following:
+  swig.setDefaults({ cache: false });
+  // NOTE: You should always cache templates in a production environment.
+  // Don't leave both of these to `false` in production!
 
-    // support POST: {"name":"foo","color":"red"}
-    app.use(express.json());
-    // support POST: name=foo&color=red
-    app.use(express.urlencoded());
+  // configure app to use bodyParser()
+  // this will let us get the data from a POST via
+  // POST: {"name":"foo","color":"red"} or
+  // POST: name=foo&color=red
+  app.use(bodyParser);
 
-    // gzip
-    app.use(express.compress());
+  // gzip
+  app.use(compress);
 
-    // static files, cached and expire in 30 days
-    // change path / to /public if need dynamic web
-    app.use("/", express.static(__dirname + '/public', {maxAge: 2592000000}));
+  // static files, cached and expire in 30 days
+  // change path / to /public if need dynamic web
+  app.use("/", serveStatic(__dirname + '/public', {maxAge: 2592000000}));
 
-    // mime
-    express.static.mime.define({'application/x-web-app-manifest+json': ['webapp']});
-    express.static.mime.define({'text/cache-manifest': ['appcache']});
-    express.static.mime.define({'image/x-icon': ['ico']});
-    // express.static.mime.define({'audio/ogg': ['ogg']});
-    // express.static.mime.define({'audio/mp4': ['m4a']});
+  // mime
+  express.static.mime.define({'application/x-web-app-manifest+json': ['webapp']});
+  express.static.mime.define({'text/cache-manifest': ['appcache']});
+  express.static.mime.define({'image/x-icon': ['ico']});
+  // express.static.mime.define({'audio/ogg': ['ogg']});
+  // express.static.mime.define({'audio/mp4': ['m4a']});
 
-    // error
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
-});
+  // error
+  app.use(errorHandler({
+      dumpExceptions: true,
+      showStack: true
+  }));
 
-// routes
-// enable route if need dynamic web
-/*
-app.get('/', function(req, res) {
-    res.render('index.html', {foo:'bar'});
-});
-*/
+  var router = express.Router();
+  // routes
+  // enable route if need dynamic web
+  /*
+  router.get('/', function(req, res) {
+      res.render('index.html', {foo:'bar'});
+  });
+  */
+  // REGISTER OUR ROUTES
+  app.use('/', router);
 
-// port
-var app_port = process.env.VCAP_APP_PORT || 8000;
-app.listen(app_port, function() {
-    console.log("Listening on " + app_port);
-});
+  // port
+  var app_port = process.env.VCAP_APP_PORT || 8000;
+  app.listen(app_port, function() {
+      console.log("Listening on " + app_port);
+  });
+})();
