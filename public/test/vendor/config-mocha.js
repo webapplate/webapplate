f93186fc1c8adc9198320e5dd643b159ea63b917
gasolin <gasolin@gmail.com>
2014-06-20 17:35:10 +0800

    failback config-mocha.js

--- public/test/vendor/config-mocha.js
+++
@@ -1,5 +0,0 @@
-mocha.setup({ui:'tdd'})
-var window.assert = window.chai.assert;
-var window.expect = window.chai.expect;
-// Note that should has to be executed
-var window.should = window.chai.should();
