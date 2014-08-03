34e50efedd08a3084e2269c89f6480f0205f8b90
gasolin <gasolin@gmail.com>
2014-08-04 00:25:40 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 // enable route if need dynamic web
 /*
 router.get('/', function(req, res) {
-  res.render('index.html', { foo: 'bar' });
+  res.render('index.html', {foo: 'bar'});
 });
 */
 
