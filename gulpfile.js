443d8a09510e5ac1c259db1a2e121e8da2c658fc
gasolin <gasolin@gmail.com>
2015-03-24 16:15:08 +0800

    gulp add jscs into lint command

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp');
 var clean = require('gulp-clean');
 var jsdoc = require('gulp-jsdoc');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var noop = function() {};
+var stylish = require('gulp-jscs-stylish');
 // inherit center configs
 var webapplateConfigs = require('./config');
 
@@ -29,14 +32,23 @@ gulp.task('jsdoc', function() {
     .pipe(jsdoc('./docs'));
 });
 
+var lintSources = ['*.js', 'routes/**/*.js', options.param.src + '/**/*.js'];
 /**
  * Runs JSLint on all javascript files found in the app dir.
  */
-gulp.task('lint', function() {
-  return gulp.src(['*.js', 'routes/**/*.js', options.param.src + '/**/*.js'])
-    .pipe(jshint())
+gulp.task('jslint', function() {
+  return gulp.src(lintSources)
+    .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(jshint.reporter('fail'));
 });
 
+gulp.task('jscs', function() {
+  gulp.src(lintSources)
+    .pipe(jscs())
+    .on('error', noop) // don't stop on error
+    .pipe(stylish());
+});
+
+gulp.task('lint', ['jslint', 'jscs']);
 gulp.task('docs', ['clean-jsdoc', 'lint', 'jsdoc']);
