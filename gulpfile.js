9d576bf72321cae03bc5646f65721f28fc6dc27a
gasolin <gasolin@gmail.com>
2015-03-24 16:06:17 +0800

    add clean-jsdoc and jshint gulp command

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 /* global require */
 'use strict';
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var jsdoc = require('gulp-jsdoc');
+var jshint = require('gulp-jshint');
 // inherit center configs
 var webapplateConfigs = require('./config');
 
@@ -17,9 +19,24 @@ var options = {
   }
 };
 
+gulp.task('clean-jsdoc', function() {
+  return gulp.src('docs', {read: false})
+    .pipe(clean());
+});
+
 gulp.task('jsdoc', function() {
   return gulp.src(options.param.src + '/js/*.js')
     .pipe(jsdoc('./docs'));
 });
 
-gulp.task('docs', ['jsdoc']);
+/**
+ * Runs JSLint on all javascript files found in the app dir.
+ */
+gulp.task('lint', function() {
+  return gulp.src(['*.js', 'routes/**/*.js', options.param.src + '/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('docs', ['clean-jsdoc', 'lint', 'jsdoc']);
