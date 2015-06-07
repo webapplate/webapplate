6e4d1fa2b2ee0ab671921f0e8afd576214c284b7
gasolin <gasolin@gmail.com>
2015-06-07 21:13:23 +0800

    Update 2015-06-07T13:13:17.339Z

--- public/test/vendor/mocha-config.js
+++
@@ -1,5 +0,0 @@
-mocha.setup({ui:'tdd'});
-var assert = chai.assert;
-var expect = chai.expect;
-// Note that should has to be executed
-var should = chai.should();
--- routes/urls.js
+++
@@ -1,7 +0,0 @@
-/* jshint node: true */
-'use strict';
-// REGISTER OUR ROUTES
-module.exports = function(app) {
-  app.use('/', require('./index'));
-  app.use('/api/1/', require('./api'));
-};
